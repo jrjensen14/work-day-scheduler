e7a4863478fc59f9f7ce9c3dc3da1ae34289b4ee
JRJ <jrjensen14@gmail.com>
2021-03-29 15:42:39 -0500

    time blocks working

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
-$("#currentDay").text(moment().format("MMMM Do, YYYY"));
+$(document).ready(function() {
+  $(".saveBtn").on("click", function() {
+    var value = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    
+    localStorage.setItem(time, value);
+  });
 
-var time = moment().format("LL");
-console.log(time);
+  function hourUpdate() {
+    var currenttime = moment().hours();
 
-// Create Task Funcion
+    $(".time-block").each(function() {
+      var blockhour = parseInt($(this).attr("id").split("-")[1]);
 
-// Time block color for past, present, future
-var auditTask = function() {
-    console.log("test");
-};
+      if (blockhour < currenttime) {
+        $(this).addClass("past");
+      } 
+      else if (blockhour === currenttime) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      }
+      else {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
+  hourUpdate();
+
+  var interval = setInterval(hourUpdate, 15000);
+
+  $("#hour-8 .description").val(localStorage.getItem("hour-8"));
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+  $("#currentDay").text(moment().format("dddd, MMMM, Do"));
+
+
+});
